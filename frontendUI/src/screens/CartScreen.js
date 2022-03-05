78f493533f07311455233f5e29747d8c2e0ac6cf
Ariel Pinto <arielp@goja.com>
2022-03-04 22:59:59 -0400

    create reducer and action for delete cartItem

--- frontendUI/src/screens/CartScreen.js
+++ frontendUI/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link, useParams, useSearchParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions'; 
+import { addToCart, removeFromCart } from '../actions/cartActions'; 
 
 export default function CartScreen({location}){
 
@@ -24,7 +24,7 @@ export default function CartScreen({location}){
     }, [dispatch, productId, qty]);
     
     const removeFromCartHandler = (id) => {
-        console.log('remove', id);
+        dispatch(removeFromCart(id));
     }
     
     const checkoutHandler = () => {
