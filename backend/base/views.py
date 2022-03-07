15a324894a11615d05b628a8f2a8e7f52cc085ad
Ariel Pinto <arielp@goja.com>
2022-03-07 11:00:18 -0400

    Add User serializer and endpoint for users/profile

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,14 +5,39 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 # Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # data['ussername'] = self.user.username
+        # data['email'] = self.user.email
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
+    #     token['username'] = user.username
+    #     token['message'] = 'hello world'
+    #     return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        '/api.products',
+        '/api/products',
         '/api/products/create/',
         '/api/products/upload'
     ]
@@ -22,6 +47,12 @@ def getRoutes(request):
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
 @api_view(['GET'])
