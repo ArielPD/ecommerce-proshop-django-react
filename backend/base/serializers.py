5f1bc7a45bd971d774aa25b46608de895a965090
Ariel Pinto <arielp@goja.com>
2022-03-09 17:13:23 -0400

    Create endpoint for get order by ID in backend

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -65,7 +65,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def get_shippingAddress(self, obj):
         try:
-            address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+            address = ShippingAddressSerializer(obj.shippingaddress, many=False).data
         except:
             address = False
         return address
@@ -74,4 +74,3 @@ class OrderSerializer(serializers.ModelSerializer):
         user = obj.user
         serializer = UserSerializer(user, many=False)
         return serializer.data
-    
