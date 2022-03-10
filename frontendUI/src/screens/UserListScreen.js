332191f00475520f5b83675067f9e753cad85812
Ariel Pinto <arielp@goja.com>
2022-03-10 16:59:37 -0400

    Create endpoint for delete user, add constant, action, reducer, store and integrate with UI

--- frontendUI/src/screens/UserListScreen.js
+++ frontendUI/src/screens/UserListScreen.js
@@ -5,7 +5,7 @@ import { Table, Button} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { listUsers } from '../actions/userActions';
+import { listUsers, deleteUser } from '../actions/userActions';
 
 export default function UserListScreen() {
 
@@ -18,16 +18,21 @@ export default function UserListScreen() {
     const userLogin = useSelector(state => state.userLogin);
     const {userInfo} = userLogin;
 
+    const userDelete = useSelector(state => state.userDelete);
+    const {success:successDelete } = userDelete;
+
     useEffect(() => {
         if (userInfo && userInfo.isAdmin) {
             dispatch(listUsers());
         } else {
             navigate('/login');
         }
-    }, [dispatch, navigate, userInfo])
+    }, [dispatch, navigate, userInfo, successDelete])
 
     const deleteHandler = (id) => {
-        console.log('delete', id);
+        if (window.confirm('Are you sure you want to delete this user?')) {
+            dispatch(deleteUser(id));
+        }
     }
 
     return (
