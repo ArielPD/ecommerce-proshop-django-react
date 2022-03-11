d29591193e572d1b94c993c967cab169acf8e72f
Ariel Pinto <arielp@goja.com>
2022-03-11 13:01:57 -0400

    Create constants, reducer, action, and integrate with UI for create products

--- frontendUI/src/screens/ProductListScreen.js
+++ frontendUI/src/screens/ProductListScreen.js
@@ -5,7 +5,8 @@ import { Table, Button, Row, Col} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { listProducts, deleteProduct } from '../actions/productActions';
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions';
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
 
 export default function ProductListScreen() {
 
@@ -17,17 +18,26 @@ export default function ProductListScreen() {
 
     const productDelete = useSelector(state => state.productDelete);
     const {loading:loadingDelete, error:errorDelete, success:successDelete} = productDelete;
+    
+    const productCreate = useSelector(state => state.productCreate);
+    const {loading:loadingCreate, error:errorCreate, success:successCreate, product:productedCreate} = productCreate;
 
     const userLogin = useSelector(state => state.userLogin);
     const {userInfo} = userLogin;
 
     useEffect(() => {
-        if (userInfo && userInfo.isAdmin) {
-            dispatch(listProducts());
-        } else {
+        
+        if (!userInfo.isAdmin) {
             navigate('/login');
         }
-    }, [dispatch, navigate, userInfo, successDelete])
+        
+        if (successCreate) {
+            navigate(`/admin/product/${productedCreate._id}/edit/`);
+        } else {
+            dispatch(listProducts());
+        }
+        dispatch({ type: PRODUCT_CREATE_RESET })
+    }, [dispatch, navigate, userInfo, successDelete, successCreate, productedCreate])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure you want to delete this product?')) {
@@ -36,7 +46,7 @@ export default function ProductListScreen() {
     }
 
     const createProductHandler = (product) => {
-
+        dispatch(createProduct());
     }
 
     return (
@@ -54,6 +64,8 @@ export default function ProductListScreen() {
 
             {loadingDelete && <Loader />}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+            {loadingCreate && <Loader />}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
 
             {loading
              ? (<Loader />)
--- frontendUI/src/store.js
+++ frontendUI/src/store.js
@@ -1,7 +1,12 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducers, productDetailsReducer, productDeleteReducer  } from './reducers/productReducers';
+import { 
+    productListReducers, 
+    productDetailsReducer, 
+    productDeleteReducer,
+    productCreateReducer,  
+} from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import { 
     userLoginReducer, 
@@ -18,6 +23,7 @@ const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducer,
     productDelete: productDeleteReducer,
+    productCreate: productCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userLoginReducer,
