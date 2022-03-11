d38decb24d87fec089474ebbc230a9f9c8b77848
Ariel Pinto <arielp@goja.com>
2022-03-11 14:43:19 -0400

    Create Product Edit screen in UI

--- frontendUI/src/App.js
+++ frontendUI/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             <Route path='/admin/userList' element={<UserListScreen />}/>
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
             <Route path='/admin/productList/' element={<ProductListScreen />}/>
+            <Route path='/admin/product/:id/edit/' element={<ProductEditScreen />} />
             <Route path="/product/:id"  element={<ProductScreen />} />
             <Route path='/cart/:id' element={<CartScreen />} />
             <Route path='/cart/' element={<CartScreen />} />
