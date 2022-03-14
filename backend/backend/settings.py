043f346176b38c2a98edaebd79451d8cd6afa83a
Ariel Pinto <arielp@goja.com>
2022-03-14 15:42:45 -0400

    Merging and configure React build for deploy with Django project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -102,7 +103,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'buildDeployUI/')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -167,7 +170,8 @@ STATIC_URL = 'static/'
 MEDIA_URL= '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'buildDeployUI/static'
 ]
 
 MEDIA_ROOT = 'static/images'
